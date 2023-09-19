9d9cc5b0245afb7bb23b829db22495c0821ca879
Bhupender98 <108581770+Bhupender98@users.noreply.github.com>
2023-09-19 11:44:05 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongooose = require('mongoose');
 // -----------connection to mongodb-------------------//
-mongooose.connect('mongodb+srv://divyagaurav:divyagaurav@cluster0.dmvvsbj.mongodb.net/?retryWrites=true&w=majority');
-// mongooose.connect('mongodb://localhost/HospitalAPI');
+mongooose.connect('mongodb+srv://bhupander17440:<password>@cluster0.biz9oc0.mongodb.net/?retryWrites=true&w=majority');
 
 // ---------------establish connection---------------//
 const db = mongooose.connection;
