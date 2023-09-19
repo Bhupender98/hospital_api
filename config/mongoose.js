32bfb5878c17bca68732f6f7f9378937ea79d878
Bhupender98 <108581770+Bhupender98@users.noreply.github.com>
2023-09-19 12:09:16 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongooose = require('mongoose');
 // -----------connection to mongodb-------------------//
-mongooose.connect('mongodb+srv://bhupander17440:<password>@cluster0.biz9oc0.mongodb.net/?retryWrites=true&w=majority');
+mongooose.connect('mongodb+srv://bhupander17440:<abc@123456>@cluster0.voeib1v.mongodb.net/?retryWrites=true&w=majority');
 
 // ---------------establish connection---------------//
 const db = mongooose.connection;
